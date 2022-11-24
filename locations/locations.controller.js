9463feb459af14192dca2831b3bc1e4df05af5d7
ManonFlt <manon.flament@edu.devinci.fr>
2022-11-24 18:35:05 +0100

    feat(6): Get All & Get One

--- locations/locations.controller.js
+++ locations/locations.controller.js
@@ -1,14 +1,43 @@
 // This file is used to map API calls (Presentation Layer) with the
 // Business-Logic layer
 
+//const filmingLocations = require('./lieux-de-tournage-a-paris.json')
+
 const router = require('express').Router()
 const locationsService = require('./locations.service')
 
-router.get('/locations', (req, res) => {
-	return res.status(200).send({locations: []})
+router.get('/', (req, res) => {
+	return res.status(200).send('Hello World!')
 })
-router.get('/hello', (req, res) => {
-	return res.status(200).send('Hello world')
+
+router.get('/locations', async(req, res, next) => {
+	//return res.status(200).send(locationsService.getAll({}))
+	try {
+		const allLocs = await locationsService.getAll({});
+		res.json(allLocs);
+	} catch(error) {
+		next(error);
+	}
 })
+
+router.get('/locations/:id', async(req, res,next) => {
+	//return res.status(200).send(locationsService.findOne(req.params.id))
+	try {
+		const { id } = req.params;
+		const location = await locationsService.findOne({
+			_id: id,
+		});
+
+		if (!location) {
+			const error = new Error('Cannot find location');
+			return next(error);
+		}
+
+		res.json(location);
+	} catch (error) {
+		next(error);
+	}
+})
+
 
 module.exports = router
--- locations/locations.service.js
+++ locations/locations.service.js
@@ -2,8 +2,35 @@
 
 const Location = require('./locations.model')
 
-function findAll () {
+function findAll() {
 	return [1,2,3,4]
 }
 
+async function create(){
+
+}
+
+async function update(id, modification){
+
+}
+
+function getAll(){
+	//return Location[1,2,3,4]
+	return Location.find({})
+}
+
+async function getOne(){
+
+}
+
+function findOne(id){
+	//return Location.findOne({sourceLocationId: id})
+	return Location.findOne({sourceLocationId: id})
+}
+
 module.exports.findAll = findAll
+module.exports.findOne = findOne
+module.exports.create = create
+module.exports.update = update
+module.exports.getAll = getAll
+module.exports.getOne = getOne
